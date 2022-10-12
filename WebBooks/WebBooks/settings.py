6b8768cf8e9cbf101a083fb2bf589e3ba30a8766
KonstantinViv <vivivova2047@gmail.com>
2022-10-12 17:42:37 +0300

    33

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -137,3 +137,19 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Упрощенная обработка статическая файлов.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#***********************************
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+#***********************************
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#***************************
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+#*********************************
